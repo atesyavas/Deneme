00f1b8ecfd87f9449342226df42e89e8ba1085c2
Seray Fırıncıoğulları <158742686+serayfirinciogullari@users.noreply.github.com>
2024-03-07 13:03:35 +0300

    uyarı mesajının yeri değiştirildi

--- sezar.c
+++ sezar.c
@@ -1,53 +1,39 @@
 #include <stdio.h>
 
-
-void sezarSifrele(char metin[], int kaydirma) // şifreleme fonksiyonu oluşturma
-{
-    
+void sezarSifrele(char metin[], int kaydirma) {
     int i = 0;
 
-    while (metin[i] != '\0') 
-    {
+    while (metin[i] != '\0') {
         //küçük harfi şifreleme
-        if (metin[i] >= 'a' && metin[i] <= 'z')
-        {
+        if (metin[i] >= 'a' && metin[i] <= 'z') {
             metin[i] = 'a' + (metin[i] - 'a' + kaydirma) % 26; 
         }
-            
         // Büyük harfi şifreleme
         else if (metin[i] >= 'A' && metin[i] <= 'Z') {
             metin[i] = 'A' + (metin[i] - 'A' + kaydirma) % 26;
-
-        //doğal sayıları şifreleme
-        else if(metin[i]>='0'&&metin[i]<='9')
-        {
-            metin[i]='0'+(metin[i]-'0'+kaydirma)%10;
         }
-            
+        //sayıları şifreleme
+        else if(metin[i]>='0'&&metin[i]<='9') {
+            metin[i]='0'+(metin[i]-'0'+kaydirma)%10;
         }
         i++;
     }
 }
 
-void sezarCoz(char sifreli[], int kaydir ) //şifre çözme fonksiyonu oluşturma
-{
+void sezarCoz(char sifreli[], int kaydir) {
     int j=0;
 
-    while(sifreli[j]!='\0')
-    {
+    while(sifreli[j]!='\0') {
         //küçük harfin şifresini çözme
-        if(sifreli[j]>='a'&&sifreli[j]<='z')
-        {
+        if(sifreli[j]>='a'&&sifreli[j]<='z') {
             sifreli[j]='z'+(sifreli[j]-'z'-kaydir)%26;
         }
         //büyük harfin şifresini çözme
-        else if(sifreli[j]>='A'&&sifreli[j]<='Z')
-        {
+        else if(sifreli[j]>='A'&&sifreli[j]<='Z') {
             sifreli[j]='Z'+(sifreli[j]-'Z'-kaydir)%26;
         }
         //sayıların şifresini çözme
-        else if(sifreli[j]>='0'&&sifreli[j]<='9')
-        {
+        else if(sifreli[j]>='0'&&sifreli[j]<='9') {
             sifreli[j]='9'+(sifreli[j]-'9'-kaydir)%10;
         }
         
@@ -58,43 +44,44 @@ void sezarCoz(char sifreli[], int kaydir ) //şifre çözme fonksiyonu oluşturm
 int main() {
     char metin[100];
     int kaydirma;
+    char dosyaAdi[50];
 
-    // Kullanıcıyı uyarma
-    printf("\n------------------------------------------------------");
-    printf("\nNOT:Metin yalnizca dogal sayi ve harflerden olusmalidir!\n");
-    printf("-------------------------------------------------------\n\n");
     
+
     // Kullanıcıdan dosya adını alma
     printf("Dosya adini girin: ");
     scanf("%s", dosyaAdi);
-    
+
+    // Kullanıcı uyarısı
+    printf("\n------------------------------------------------------");
+    printf("\nNOT: Metin yalnizca dogal sayi ve harflerden olusmalidir!\n");
+    printf("-------------------------------------------------------\n\n");
+
     // Kullanıcıdan metni ve kaydırma miktarı alma
     printf("Sifrelemek istediginiz metni giriniz--> ");
     scanf("%s", metin);
     printf("Sifre kaydirma miktarini giriniz--> ");
     scanf("%d", &kaydirma);
 
-     // Dosyayı açma
+    // Dosyayı açma
     FILE *dosya = fopen(dosyaAdi, "w");
-    if (dosya == NULL) 
-    {
+    if (dosya == NULL) {
         printf("Dosya acilamadi!\n");
         return 1;
     }
-
-    // Şifreyi çözecek fonksiyonu çağırma
+    
+    // Şifreleyecek fonksiyonu çağırma ve dosyaya yazma
     sezarSifrele(metin, kaydirma);
-    printf("Sifrelenmis metin=> %s\n", metin);
+    fprintf(dosya, "Sifrelenmis metin: %s\n", metin);
 
-    //Şifre çözecek fonksiyonu çağırma
+    //Şifre çözecek fonksiyonu çağırma ve dosyaya yazma
     sezarCoz(metin,kaydirma);
-    printf("Sifresi cozulmus metin=> %s",metin);
+    fprintf(dosya, "Sifresi cozulmus metin: %s",metin);
 
     // Dosyayı kapatma
     fclose(dosya);
 
     printf("Sifrelenmis ve sifresi cozulmus metin dosyaya yazildi.\n");
-
 
     return 0;
 }
