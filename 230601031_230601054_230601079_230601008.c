9556de71d19b8d50c594853f59e259694f0f87bf
atesyavas <160889240+atesyavas@users.noreply.github.com>
2024-03-14 01:32:15 +0300

    Update 230601031_230601054_230601079_230601008.c
    kodumuza 4. ve 5. seçenek olarak vigenere şifreleme ve çözmeyi ekledim

--- 230601031_230601054_230601079_230601008.c
+++ 230601031_230601054_230601079_230601008.c
@@ -1,57 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+
+void vigenere_encrypt(char *text, char *key) {
+    int text_len = strlen(text);
+    int key_len = strlen(key);
+    int i;
+
+    for (i = 0; i < text_len; i++) {
+        if (isalpha(text[i])) {
+            char base = islower(text[i]) ? 'a' : 'A';
+            text[i] = ((text[i] - base + key[i % key_len] - 'a') % 26) + base;
+        }
+    }
+}
+
+void vigenere_decrypt(char *text, char *key) {
+    int text_len = strlen(text);
+    int key_len = strlen(key);
+    int i;
+
+    for (i = 0; i < text_len; i++) {
+        if (isalpha(text[i])) {
+            char base = islower(text[i]) ? 'a' : 'A';
+            text[i] = ((text[i] - base - (key[i % key_len] - 'a') + 26) % 26) + base;
+        }
+    }
+}
 
 void sezarSifrele(char metin[], int kaydirma) {
     int i = 0;
 
     while (metin[i] != '\0') {
-        //küçük harfi şifreleme
-        if (metin[i] >= 'a' && metin[i] <= 'z') {
-            metin[i] = 'a' + (metin[i] - 'a' + kaydirma) % 26; 
-        }
-        // Büyük harfi şifreleme
-        else if (metin[i] >= 'A' && metin[i] <= 'Z') {
-            metin[i] = 'A' + (metin[i] - 'A' + kaydirma) % 26;
-        }
-        //sayıları şifreleme
-        else if(metin[i]>='0'&&metin[i]<='9') {
-            metin[i]='0'+(metin[i]-'0'+kaydirma)%10;
+        if (isalpha(metin[i])) {
+            char base = islower(metin[i]) ? 'a' : 'A';
+            metin[i] = ((metin[i] - base + kaydirma) % 26) + base;
+        } else if (isdigit(metin[i])) {
+            metin[i] = ((metin[i] - '0' + kaydirma) % 10) + '0';
         }
         i++;
     }
 }
 
 void sezarCoz(char sifreli[], int kaydir) {
-    int j=0;
+    int j = 0;
 
-    while(sifreli[j]!='\0') {
-        //küçük harfin şifresini çözme
-        if(sifreli[j]>='a'&&sifreli[j]<='z') {
-            sifreli[j]='z'+(sifreli[j]-'z'-kaydir)%26;
+    while (sifreli[j] != '\0') {
+        if (isalpha(sifreli[j])) {
+            char base = islower(sifreli[j]) ? 'a' : 'A';
+            sifreli[j] = ((sifreli[j] - base - kaydir + 26) % 26) + base;
+        } else if (isdigit(sifreli[j])) {
+            sifreli[j] = ((sifreli[j] - '0' - kaydir + 10) % 10) + '0';
         }
-        //büyük harfin şifresini çözme
-        else if(sifreli[j]>='A'&&sifreli[j]<='Z') {
-            sifreli[j]='Z'+(sifreli[j]-'Z'-kaydir)%26;
-        }
-        //sayıların şifresini çözme
-        else if(sifreli[j]>='0'&&sifreli[j]<='9') {
-            sifreli[j]='9'+(sifreli[j]-'9'-kaydir)%10;
-        }
-        
         j++;
     }
 }
 
-
-
 void atbashSifreleme(char *text) {
     int i;
     for (i = 0; i < strlen(text); i++) {
-        if (text[i] >= 'A' && text[i] <= 'Z') {
-            text[i] = 'Z' - (text[i] - 'A');
-        } else if (text[i] >= 'a' && text[i] <= 'z') {
-            text[i] = 'z' - (text[i] - 'a');
+        if (isalpha(text[i])) {
+            char base = islower(text[i]) ? 'a' : 'A';
+            text[i] = base + ('z' - (tolower(text[i]) - 'a'));
+        } else if (isdigit(text[i])) {
+            text[i] = '9' - (text[i] - '0');
         }
     }
 }
@@ -61,80 +74,68 @@ int main() {
     char dosyaAdi[50];
     int secim, kaydirma;
 
-    // Kullanıcıdan dosya adını alma
     printf("Dosya adini girin: ");
     scanf("%s", dosyaAdi);
 
-    // Kullanıcıya seçenekleri sunma
     printf("\nSecim yapiniz:\n");
     printf("1. Sezar sifreleme\n");
     printf("2. Atbash sifreleme\n");
     printf("3. Sezar sifre cozme\n");
+    printf("4. Vigenere sifreleme\n");
+    printf("5. Vigenere sifre cozme\n");
     printf("Secim: ");
     scanf("%d", &secim);
 
-    // Kullanıcıdan metni alma
     printf("\nMetni giriniz: ");
     scanf("%s", metin);
 
-    // Cezar Şifreleme seçildiyse
-    if (secim == 1) {
-        // Kullanıcıdan kaydırma miktarını alma
-        printf("Sifre kaydirma miktarini giriniz: ");
-        scanf("%d", &kaydirma);
-        
-        // Cezar Şifreleme işlemini yapma
-        sezarSifrele(metin, kaydirma);
-
-        // Sonucu dosyaya yazma
-        FILE *dosya = fopen(dosyaAdi, "w");
-        if (dosya == NULL) {
-            printf("Dosya acilamadi!\n");
-            return 1;
-        }
-        fprintf(dosya, "Sifrelenmis metin: %s", metin);
-        fclose(dosya);
-
-        printf("Sezar sifreleme ile metin dosyaya yazildi.\n");
+    FILE *dosya = fopen(dosyaAdi, "w");
+    if (dosya == NULL) {
+        printf("Dosya acilamadi!\n");
+        return 1;
     }
 
-    else if(secim==2)
-    {
-        atbashSifreleme(metin);
-
-        // Sonucu dosyaya yazma
-        FILE *dosya = fopen(dosyaAdi, "w");
-        if (dosya == NULL) 
-        {
-            printf("Dosya acilamadi!\n");
-            return 1;
-        }
-        fprintf(dosya, "Sifresi cozulmus metin: %s", metin);
-        fclose(dosya);
-
-        printf("Atbash sifre cozme ile metin dosyaya yazildi.\n");
-    }   
-        
-
-    else if(secim==3)
-    {
-        // Kullanıcıdan kaydırma miktarını alma
-        printf("Sifre kaydirma miktarini giriniz: ");
-        scanf("%d", &kaydirma);
-        
-        // Cezar Şifreleme işlemini yapma
-        sezarCoz(metin, kaydirma);
-
-        // Sonucu dosyaya yazma
-        FILE *dosya = fopen(dosyaAdi, "w");
-        if (dosya == NULL) {
-            printf("Dosya acilamadi!\n");
-            return 1;
-        }
-        fprintf(dosya, "Metnin orijinal hali: %s", metin);
-        fclose(dosya);
-
-        printf("Sezar ile sifrelenen metin orijinal haline cevrildi.\n");
+    switch (secim) {
+        case 1:
+            printf("Sifre kaydirma miktarini giriniz: ");
+            scanf("%d", &kaydirma);
+            sezarSifrele(metin, kaydirma);
+            fprintf(dosya, "Sifrelenmis metin: %s", metin);
+            printf("Sezar sifreleme ile metin dosyaya yazildi.\n");
+            break;
+        case 2:
+            atbashSifreleme(metin);
+            fprintf(dosya, "Sifresi cozulmus metin: %s", metin);
+            printf("Atbash sifre cozme ile metin dosyaya yazildi.\n");
+            break;
+        case 3:
+            printf("Sifre kaydirma miktarini giriniz: ");
+            scanf("%d", &kaydirma);
+            sezarCoz(metin, kaydirma);
+            fprintf(dosya, "Metnin orijinal hali: %s", metin);
+            printf("Sezar ile sifrelenen metin orijinal haline cevrildi.\n");
+            break;
+        case 4:
+            printf("Vigenere anahtarini giriniz: ");
+            char anahtar[100];
+            scanf("%s", anahtar);
+            vigenere_encrypt(metin, anahtar);
+            fprintf(dosya, "Vigenere sifrelenmis metin: %s", metin);
+            printf("Vigenere sifreleme ile metin dosyaya yazildi.\n");
+            break;
+        case 5:
+            printf("Vigenere anahtarini giriniz: ");
+            char anahtar2[100];
+            scanf("%s", anahtar2);
+            vigenere_decrypt(metin, anahtar2);
+            fprintf(dosya, "Vigenere sifresi cozulmus metin: %s", metin);
+            printf("Vigenere sifre cozme ile metin dosyaya yazildi.\n");
+            break;
+        default:
+            printf("Gecersiz secim!\n");
+            break;
     }
 
-    }
+    fclose(dosya);
+    return 0;
+}
